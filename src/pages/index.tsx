5c8234f9cda4548f39f2391aa91c57ac911d0741
Edney Fillipi <e.fillipi@gmail.com>
2022-02-10 06:20:25 -0300

    :sparkles: init page

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+// @flow
+//fsc
+import { GetServerSideProps, NextPage } from 'next';
+import * as React from 'react';
+
+type Props = {
+  name: string
+};
+
+const PaginaPage: NextPage<Props> = (props) => {
+  return (
+    <h1>
+      Hello Edney {props.name}
+    </h1>
+  );
+};
+
+export const getServerSideProps : GetServerSideProps = async(conext)=>{
+  return{
+    props:{
+      name : 'Full Cycle'
+  }
+}
+
+}
+
+export default PaginaPage
---
+++ src/pages/tweets.tsx
@@ -0,0 +1,14 @@
+// @flow
+import { NextPage } from 'next';
+import * as React from 'react';
+
+const TweetsPage:NextPage = () => {
+  return (
+    <div>
+      <h1>Tweets</h1>
+  <h4>Listagem Tweets</h4>
+    </div>
+  );
+};
+
+export default TweetsPage
