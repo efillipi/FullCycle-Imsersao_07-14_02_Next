1d592c6106997030e62edabb0edde2dcecdbf3fb
Edney Fillipi <e.fillipi@gmail.com>
2022-02-11 11:07:22 -0300

    :sparkles: theme dark/light

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,19 +1,20 @@
-/* eslint-disable no-shadow */
 export enum Themes {
-  LIGHT = 'light',
-  DARK = 'dark',
+  LIGHT = "light",
+  DARK = "dark",
 }
 
 export function toggleTheme() {
-  localStorage.theme =
-    localStorage.theme === Themes.DARK || !localStorage.theme
-      ? Themes.LIGHT
-      : Themes.DARK;
+  if (!localStorage.theme) {
+    localStorage.theme = Themes.DARK;
+  } else {
+    localStorage.theme =
+      localStorage.theme === Themes.DARK ? Themes.LIGHT : Themes.DARK;
+  }
   loadTheme();
 }
 
 export function loadTheme() {
-  if (localStorage.theme === Themes.DARK || !('theme' in localStorage)) {
+  if (localStorage.theme === Themes.DARK || !("theme" in localStorage)) {
     document.documentElement.classList.add(Themes.DARK);
   } else {
     document.documentElement.classList.remove(Themes.DARK);
